d668ef483b1eb577a1ffe1a215cbbd44ffa0d018
Tony Kalalian <120026287+tonykalalian@users.noreply.github.com>
2023-09-11 17:58:51 +0300

    Managing Global State

--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -1,18 +1,64 @@
 const FeedbackData = [
   {
     id: 1,
-    rating: 10,
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+    rating: 9,
+    text: "Outstanding service! The team exceeded my expectations.",
   },
   {
     id: 2,
-    rating: 9,
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+    rating: 8,
+    text: "Very professional and responsive. I'm satisfied with their support.",
   },
   {
     id: 3,
+    rating: 7,
+    text: "Good experience overall. Would consider using their services again.",
+  },
+  {
+    id: 4,
+    rating: 6,
+    text: "Decent service, but there's room for improvement.",
+  },
+  {
+    id: 5,
+    rating: 5,
+    text: "Satisfactory experience. Met my expectations.",
+  },
+  {
+    id: 6,
+    rating: 9,
+    text: "Exceptional quality and attention to detail. Impressed!",
+  },
+  {
+    id: 7,
     rating: 8,
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+    text: "Responsive customer support. Resolved my issues promptly.",
+  },
+  {
+    id: 8,
+    rating: 6,
+    text: "Average service. Could be better.",
+  },
+  {
+    id: 9,
+    rating: 4,
+    text: "Not satisfied with the product. Needs improvement.",
+  },
+  {
+    id: 10,
+    rating: 2,
+    text: "Terrible experience. Would not recommend.",
+  },
+  {
+    id: 11,
+    rating: 10,
+    text: "Absolutely fantastic service! Best I've ever experienced.",
+  },
+  {
+    id: 12,
+    rating: 9,
+    text: "Very reliable and professional. Highly recommended.",
   },
 ];
+
 export default FeedbackData;
