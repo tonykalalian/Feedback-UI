0e3f73bd60164aba6ecabce049ab7372129a5ab7
Tony Kalalian <120026287+tonykalalian@users.noreply.github.com>
2023-09-11 17:36:12 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+
 import Header from "./components/Header";
 import FeedbackItem from "./components/FeedbackItem";
+import FeedbackData from "./data/FeedbackData";
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
   return (
     <>
       <Header />
